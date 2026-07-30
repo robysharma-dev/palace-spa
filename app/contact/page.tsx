db1797f0a48372b86a284d7d89b1428665bb27bc
Roby Sharma <robssvesi@gmail.com>
2026-07-30 13:31:06 +0530

    Fix TypeScript allowFullScreen error in contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -28,7 +28,7 @@ export default function Contact() {
           <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8166567169055!2d103.852445!3d1.298285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31da19a5bed7bdff%3A0xedc713x9we0!2s116%20Middle%20Rd%2C%20%2302-01%20ICB%20Enterprise%20House%2C%20Singapore%20188972!5e0!3m2!1sen!2ssg!4v1650000000000!5m2!1sen!2ssg" 
             className="w-full h-full border-0" 
-            allowFullScreen="" 
+            allowFullScreen={true} 
             loading="lazy"
           ></iframe>
         </div>
